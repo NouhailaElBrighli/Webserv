3be56f5970bccafaa0adf7ecb3091cd94a995c40
hajar-saidi <hajarsaidi32@gmail.com>
2023-05-18 21:01:23 +0100

    i checed the extention of the file and started env for  execve

--- WSNetworking/Headers/Cgi.hpp
+++ WSNetworking/Headers/Cgi.hpp
@@ -6,7 +6,7 @@
 /*   By: hsaidi <hsaidi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 11:33:49 by hsaidi            #+#    #+#             */
-/*   Updated: 2023/05/15 11:19:40 by hsaidi           ###   ########.fr       */
+/*   Updated: 2023/05/18 18:02:12 by hsaidi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,16 @@ class Cgi
         MainClient *main_client;
         vector<ConfigLocationParser *> config_location_parser;
         map<string, string> cgi_env;
+        // std::string &filename;
+
     public:
         Cgi(MainClient *main_client, vector<ConfigLocationParser *>config_location_parser);
+        // Cgi(std::string &filename);
         ~Cgi();
         void just_print();
         void set_cgi_env();
+        // int getFileType(const std::string& filename);
+        // void readFileContents(std::string &filename) ;
     // class CgiException : public std::exception
     // {
     //     private:
@@ -37,5 +42,17 @@ class Cgi
     // };
 }; 
 
-
+// std::string script_name = std::string("SCRIPT_NAME=") + script;
+// 	std::string path = std::string("PATH_INFO=") + getValue("URL");
+// 	std::string q = std::string ("QUERY_STRING=") + query;
+// 	std::string cookie = std::string ("HTTP_COOKIE=") + getValue("Cookie");
+// 	std::string method = std::string ("REQUEST_METHOD=") + getValue("Method");
+// 	std::string script_filename = std::string ("SCRIPT_FILENAME=") + file_path;
+// 	std::string protocol = std::string ("SERVER_PROTOCOL=") + getValue("version");
+// 	std::string software = std::string ("SERVER_SOFTWARE=WEBSERV");
+// 	std::string redirect = std::string ("REDIRECT_STATUS=true");
+// 	std::string uri = std::string("REQUEST_URI=") + getValue("URL");
+// 	std::string host = std::string("HTTP_HOST=") + getValue("Host");
+// 	std::string port = std::string("SERVER_PORT=") + port_host.first;
+// 	std::string gateway = std::string("GATEWAY_INTERFACE=CGI/1.1");
 #endif // CGI_HPP
--- WSNetworking/Sources/Cgi.cpp
+++ WSNetworking/Sources/Cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: hsaidi <hsaidi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 11:38:43 by hsaidi            #+#    #+#             */
-/*   Updated: 2023/05/17 11:41:51 by hsaidi           ###   ########.fr       */
+/*   Updated: 2023/05/18 21:01:11 by hsaidi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,49 @@
 Cgi::Cgi(MainClient *main_client, vector<ConfigLocationParser *>config_location_parser){
 	this->main_client = main_client;
 	this->config_location_parser = config_location_parser;
+	
 }
+// Cgi::Cgi(std::string &filename) {
+// 	this->filename = filename;
+// }
 Cgi::~Cgi(){}
+
+
+//****** i commented this function because i will fixes something tomorrow ****
+
+// void Cgi::readFileContents(std::string &filename) 
+// {
+//     std::ifstream file(filename.c_str());
+//     if (file.is_open()) {
+//         std::string line;
+//         while (getline(file, line)) {
+//             std::cout << line << std::endl;
+//         }
+//         file.close();
+//     } else {
+//         std::cout << "Failed to open file: " << filename << std::endl;
+//     }
+// }
+
+// int Cgi::getFileType(const std::string& filename) {
+//     std::size_t dotPos = filename.rfind('.');
+//     if (dotPos != std::string::npos) {
+//         std::string extension = filename.substr(dotPos + 1);
+
+//         // Convert the extension to lowercase for case-insensitive comparison
+//         for (std::size_t i = 0; i < extension.length(); ++i) {
+//             extension[i] = std::tolower(extension[i]);
+//         }
+
+//         if (extension == "php") {
+//             return 1;
+//         } else if (extension == "py") {
+//             return 0;
+//         }
+//     }
+
+//     return -1;
+// }
 
 void Cgi::just_print()
 {
@@ -28,30 +69,32 @@ void Cgi::just_print()
 	{
 		cout << it->first << " : " << it->second << endl;          
 	}
-	// for (vector<ConfigLocationParser *>::const_iterator it
-			// = this->config_location_parser.begin(); it != this->config_location_parser.end(); it++)
-	// {   
-		// if ((*it)->get_location().find("cgi") != string::npos)
-		// {
+	for (vector<ConfigLocationParser *>::const_iterator it
+			= this->config_location_parser.begin(); it != this->config_location_parser.end(); it++)
+	{   
+		if ((*it)->get_location().find("cgi") != string::npos)
+		{
 			// cout << "location : " << (*it)->get_location() << endl;
 			// cout << "root : " << (*it)->get_root() << endl;
 			// cout << "cgi_ext_path : " << (*it)->get_cgi_ext_path(".py") << endl;
 			// cout << "cgi_ext_path : " << (*it)->get_cgi_ext_path(".sh") << endl;
 			// cout << "--------------------------------------------------------\n";
-		// }
-	// }
-	//throw std::runtime_error(str_red("cgi error"));
+		}
+		// getFileType(this->filename);
+		set_cgi_env();
+	}
+
 }
 
+// setting the cgi_env map
 void Cgi::set_cgi_env()
 {
-	if(this->main_client->get_request().find("Request-Type") != this->main_client->get_request().end())
-	{
-		this->cgi_env["REQUEST_METHOD"] = this->main_client->get_request()["Request-Type"];
-	}
-	setenv("REQUEST_METHOD", this->cgi_env["REQUEST_METHOD"].c_str(), 1);
-	setenv("SERVER_PROTOCOL", this->cgi_env["Protocol-Version"].c_str() 1);
-	setenv("PATH_INFO", this->cgi_env["Request-URI"].c_str(), 1);
-	
+	this->cgi_env["REQUEST_METHOD = "] = this->main_client->get_request("Request-Type");
+	cout<< "ppppppppppp |" <<this->cgi_env["REQUEST_METHOD ="] << endl;
+
 }
+
+// i just need to put the env in the map and then i will pass it to the execve 
+//fork and execve 
+
 
