9bb8b7c11b96e0f3d15cf1ccb55ba266d182f37f
NajmiAchraf <najmi.achraf@gmail.com>
2023-05-19 18:57:37 +0100

    close the socket if the bind failed  Changes to be committed: 	modified:   WSNetworking/Sources/BindingSocket.cpp

--- WSNetworking/Sources/BindingSocket.cpp
+++ WSNetworking/Sources/BindingSocket.cpp
@@ -7,8 +7,10 @@ BindingSocket::BindingSocket(const char *host, const char *port)
 	this->binding = bind(this->socket_listen, this->bind_address->ai_addr,
 						 this->bind_address->ai_addrlen);
 	freeaddrinfo(this->bind_address);
-	if (this->binding == -1)
+	if (this->binding == -1) {
+		close(this->socket_listen);
 		throw std::runtime_error(str_red("Binding socket failed"));
+	}
 }
 
 BindingSocket::~BindingSocket() {}
